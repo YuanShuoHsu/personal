04b9b476329141d48a32a0e5fd7b59a30f01f939
yuanshuo.hsu 許元碩 <yuanshuo.hsu@fortune.com.tw>
2022-05-15 15:08:36 +0800

    last version

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,55 @@
 let card = document.querySelector(".card");
-let card_container = document.querySelector(".card_container");
+let cardContainer = document.querySelector(".cardContainer");
 card.onclick = (e) => {
     e.stopPropagation();
     card.classList.add("active");
 };
-card_container.onclick = (e) => {
+cardContainer.onclick = (e) => {
     card.classList.remove("active");
 };
+
+let swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: {
+        enabled: true,
+        sticky: true,
+    },
+    keyboard: {
+        enabled: true,
+    },
+    mousewheel: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+let cv = document.querySelector(".cv");
+let swiperContainer = document.querySelector(".swiperContainer");
+let profile = document.querySelector(".swiperContainer .swiper");
+cv.onclick = (e) => {
+    swiperContainer.style.pointerEvents = "auto";
+    profile.style.opacity = "1";
+    card.style.pointerEvents = "none";
+    cardContainer.style.pointerEvents = "none";
+}
+
+let cvBackground = document.querySelectorAll(".swiper .swiper-wrapper .swiper-slide");
+let cvImg = document.querySelectorAll(".swiper .swiper-wrapper .swiper-slide img");
+
+cvBackground.forEach((currentBackground) => {
+    currentBackground.onclick = (e) => {
+        profile.style.opacity = "0";
+        swiperContainer.style.pointerEvents = "none";
+        card.style.pointerEvents = "auto";
+        cardContainer.style.pointerEvents = "auto";
+    };
+});
+
+cvImg.forEach((currentImg) => {
+    currentImg.onclick = (e) => {
+        e.stopPropagation();
+    };
+});
